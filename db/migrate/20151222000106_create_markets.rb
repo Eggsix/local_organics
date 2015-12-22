74b43c7aad50b08c1c48ea26a5c7d6b93f67f659
Preston <plphan206@yahoo.com>
2015-12-21 20:06:48 -0800

    markets show

---
+++ db/migrate/20151222000106_create_markets.rb
@@ -0,0 +1,14 @@
+class CreateMarkets < ActiveRecord::Migration
+  def change
+    create_table :markets do |t|
+      t.string, :name
+      t.string, :address
+      t.text, :products
+      t.string, :schedule
+      t.decimal, :latitude
+      t.decimal, :longitude
+
+      t.timestamps
+    end
+  end
+end
