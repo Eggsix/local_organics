4907b13a0fda08b91f252ac7876783c751992ee4
Preston <plphan206@yahoo.com>
2015-12-28 14:24:07 -0800

    added facebook keys to figaro gem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["facebook_app_id"], ENV["facebook_secret"],
-  		   :scope => 'email'
+  provider :facebook, ENV["facebook_app_id"], ENV["facebook_secret"]
 end
