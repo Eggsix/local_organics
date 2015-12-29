bb510fd5eb9da0819cba511c91820c1b60be46dc
Steven5210 <shuynh5210@msn.com>
2015-12-28 19:15:10 -0800

    another try at omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 	if Rails.env.production?
-  provider :facebook, ENV["facebook_app_id"], ENV["facebook_secret"],
+  provider :facebook, ENV["facebook_app_id"], ENV["facebook_secret"]
   		   {:scope => 'email', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+  	end
 end
