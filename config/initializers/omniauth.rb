8d7758cef37414db71b428c17718b937d4e04a3e
Preston <plphan206@yahoo.com>
2015-12-28 19:12:12 -0800

    facebook key revision

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["facebook_app_id"], ENV["facebook_secret"]
+  provider :facebook, "facebook_app_id", "facebook_secret"
 end
