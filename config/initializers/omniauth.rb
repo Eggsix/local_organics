060ad8d207b95ea3591e6d77e3bc978acbe8db76
Steven5210 <shuynh5210@msn.com>
2015-12-28 19:35:50 -0800

    try number 3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["facebook_app_id"], ENV["facebook_secret"]
+  provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret']
   		   {:scope => 'email', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
