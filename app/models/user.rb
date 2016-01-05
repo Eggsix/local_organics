7f417f227c849838bda453f7bce00ae1c1725344
Steven5210 <shuynh5210@msn.com>
2016-01-05 14:56:15 -0800

    fixed auth0

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
     # if we find the user with the credentials in our database, then we are going to return that user. If  # we are not able to find it, we are going to create the user
   	def self.sign_in_from_omniauth(auth)
-		find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
+		find_by(provider: auth['provider'], uid: auth['uid'].slice!(9..25)) || create_user_from_omniauth(auth)
 	end
 
 	def self.create_user_from_omniauth(auth)
