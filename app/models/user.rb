243c7658f69c1abd1969538e741e57b694aba3ca
Steven5210 <shuynh5210@msn.com>
2016-01-05 17:50:06 -0800

    UID should work, pushing heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,16 @@ class User < ActiveRecord::Base
     # if we find the user with the credentials in our database, then we are going to return that user. If  # we are not able to find it, we are going to create the user
 
   	def self.sign_in_from_omniauth(auth)
-		find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omniauth(auth)
+  		@@auth_id = auth['uid'].slice!(9..25)
+  		puts @@auth_id
+		find_by(provider: auth['provider'], uid: @@auth_id) || create_user_from_omniauth(auth)
 	end
 
 	def self.create_user_from_omniauth(auth)
-		puts "create"
-		fb_uid = auth['uid'].slice!(9..25)
+		fb_uid = auth['uid']
 		self.create(
 			provider: auth['provider'],
-			uid: auth['uid'],
+			uid: @@auth_id,
 			name: auth['info']['name'],
 			email: auth['info']['email'],
 			profile_pic: "http://graph.facebook.com/#{fb_uid}/picture?type=square"
